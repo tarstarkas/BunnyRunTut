73f453edac6333497a12960e3b0271f93df1eed6
tarstarkas <alexandercross90@gmail.com>
2017-03-23 17:34:18 -0400

    working on android

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -1,22 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BunnyController : MonoBehaviour {
 
     private Rigidbody2D myRigidBody;
+    private Animator myAnim;
+    private Collider2D myCollider;
     public float bunnyJumpForce = 500f;
+    private float bunnyHurtTime = -1;
+    public Text scoreText;
+    private float startTime;
+    private int jumpsLeft = 1;
+    public AudioSource jumpSFX;
+    public AudioSource deathSFX;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+        startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonUp("Jump"))
+
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            myRigidBody.AddForce(transform.up * bunnyJumpForce);
+            SceneManager.LoadScene("Title");
         }
-	}
+
+        if (bunnyHurtTime == -1)
+        {
+
+          if ((Input.GetButtonUp("Jump") || Input.GetButtonUp("Fire1")) && jumpsLeft > 0)
+          {
+                if(myRigidBody.velocity.y < 0)
+                {
+                    myRigidBody.velocity = Vector2.zero;
+                }
+                if(jumpsLeft == 1)
+                {
+                    myRigidBody.AddForce(transform.up * bunnyJumpForce*0.75f);
+                }
+                else
+                {
+                    myRigidBody.AddForce(transform.up * bunnyJumpForce);
+                }
+                jumpsLeft--;
+                jumpSFX.Play();
+          }
+          myAnim.SetFloat("vVelocity",    Mathf.Abs(myRigidBody.velocity.y));
+            scoreText.text = (Time.time - startTime).ToString("0.0");
+        }
+        else
+        {
+            if(Time.time > bunnyHurtTime + 2)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            foreach (PrefabSpawner prefabSpawn in FindObjectsOfType<PrefabSpawner>())
+            {
+                prefabSpawn.enabled = false;
+            }
+            foreach (MoveLeft moveLefter in FindObjectsOfType<MoveLeft>()){
+                moveLefter.enabled = false;
+            }
+            bunnyHurtTime = Time.time;
+            myAnim.SetBool("bunnyHurt", true);
+            myRigidBody.velocity = Vector2.zero;
+            myRigidBody.AddForce(transform.up * bunnyJumpForce);
+            myCollider.enabled = false;
+            deathSFX.Play();
+        }
+        else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            jumpsLeft = 2;
+        }
+    }
+
 }
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject audioOnIcon;
+    public GameObject audioOffIcon;
+
+	// Use this for initialization
+	void Start () {
+        SetSoundState();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ToggleSound()
+    {
+        if(PlayerPrefs.GetInt("Muted",0) == 0)
+        {
+            PlayerPrefs.SetInt("Muted", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Muted", 0);
+        }
+        SetSoundState();
+    }
+
+    private void SetSoundState()
+    {
+        if(PlayerPrefs.GetInt("Muted",0) == 0)
+        {
+            AudioListener.volume = 1;
+            audioOnIcon.SetActive(true);
+            audioOffIcon.SetActive(false);
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            audioOnIcon.SetActive(false);
+            audioOffIcon.SetActive(true);
+        }
+    }
+}
